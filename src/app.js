67a33cb2b5ce862b6e2040e75a437cb2b1edc974
briartedesco <88670876+briartedesco@users.noreply.github.com>
2021-10-07 19:00:16 -0400

    Edited API

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "cd173a006b0e51dac58c6d8064c94178";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
